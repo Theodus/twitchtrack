86ea1e27918cfa19f2a307a2209eca9aa2cbbe27
Theodus <theodusbutler@gmail.com>
2015-11-06 18:49:19 -0500

    reload for better reliability

--- script.js
+++ script.js
@@ -16,22 +16,6 @@ $.getJSON('/refresh', function(data){
   });
 });
 
-setInterval(function(){
-  $.getJSON('/refresh', function(data){
-    var tbody = document.getElementById("tbody");
-    data.channels.forEach(function(element, index) {
-      var tr = tbody.childNodes[index+1];
-      var td0 = tr.childNodes[0];
-      var td1 = tr.childNodes[1];
-      var td2 = tr.childNodes[2];
-      var a = td0.childNodes[0];
-      a.innerHTML = element.channel;
-      a.href = element.url;
-      var viewers = element.viewers;
-      if(viewers > 0) {
-        td1.innerHTML = element.game;
-        td2.innerHTML = element.stream;
-      }
-    });
-  });
-}, 60000);
+setTimeout(function(){
+  location.reload()
+}, 120000);
